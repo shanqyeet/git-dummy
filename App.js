f1065eac957e4281c2e6e564675b4a76f65fdd7c
Shanq Yeet Chia <shanqyeetchia@Shanqs-MacBook-Pro.local>
2018-12-18 11:34:26 +0800

    added my name in app.js

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>Wololwolwolwol Shanq Yeet Here</Text>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
