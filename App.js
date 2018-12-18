6d588a45a49ad862bb34b4b0b46d7f95f0c295d4
nakan <nakan.tangaromsuk@gmail.com>
2018-12-18 11:34:19 +0800

    change title

--- App.js
+++ App.js
@@ -5,7 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to sta
+        rt working on your appsdfakljdshgkjadsj,adj,bhsbhjkasbhjkdbhjklab!
+        aslkdglnaslkngd</Text>
       </View>
     );
   }
